522b4489704bfcaca37894e128a662d5e5592312
kevinywlai <kevinywlai@gmail.com>
2022-06-29 01:31:25 +0800

    initial commit

---
+++ src/main/java/com/fbtest/springLearningPr/SpringLearningPrApplication.java
@@ -0,0 +1,13 @@
+package com.fbtest.springLearningPr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningPrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fbtest/springLearningPr/SpringLearningPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbtest.springLearningPr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningPrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
